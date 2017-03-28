71c40ca5a5ca75f17da3a44c00d92b3f213558f4
Will Alley <willalley345@gmail.com>
2017-03-28 19:41:42 -0400

    implemented commmand line input and multiple requests

--- src/client/tcp_client.js
+++ src/client/tcp_client.js
@@ -7,45 +7,63 @@ var buffer = require('buffer');
 
 const FILEPATH = './downloads/';
 var fileIncoming = false;
+var isConnected = false;
+var serverRes = false;
+var filename = '';
 var client = new net.Socket();
 
+// if connection succeeds, send HELLO packet
 client.connect(9001, '127.0.0.1', function() {
+    isConnected = true;
     console.log('Connected\n');
     client.write('HELLO');
     console.log('sent: \'HELLO\'');
 });
 
+// only acknoledge user input after ACK has been recieved
+process.stdin.on('data', function(input) {
+    if (isConnected) {
+        if (serverRes) {
+            if (input == 'exit') {
+                client.write(input);
+            }
+            filename = input.toString().trim();
+            client.write(filename);
+            console.log('sent: \'' + filename + '\'');
+        }
+    }
+})
+
 client.on('data', function(data) {
 
     // write fileStream to downloads
     if (fileIncoming) {
-        console.log('download beginning...')
+        console.log('download beginning...');
         
         var writeStream = fs.createWriteStream(FILEPATH + 'afile.txt');
-        writeStream.write(data);
-        // BAD:
-        /* writeStream.end(data, function() {
+        writeStream.end(data, function() {
+            fileIncoming = false;
             client.write('ACK');
             console.log('...download complete')
             console.log('sent: ACK');
-        }); */
-        client.write('ACK');
-        console.log('...download complete')
-        // BAD: console.log('sent: ACK');
+        }); 
     }
     // server indicates that next data is fileStream
     else if (data == 'FTR') {
         console.log('received: FTR');
         fileIncoming = true;
     }
-    // if server responds, requests desired file
+    // if server responds, allow user input
     else if (data == 'ACK') {
         console.log('recieved: ACK');
-        client.write('afile.txt');
-        console.log('sent: \'afile.txt\'');
+        serverRes = true;
+    }
+    else {
+        console.log('recieved: \'' + data + '\'');
     }
 });
 
 client.on('close', function() {
     console.log('Connection closed');
+    process.exit();
 });
--- src/server/tcp_server.js
+++ src/server/tcp_server.js
@@ -10,57 +10,54 @@ const PORT = '9001';
 const FILEPATH = './public/';
 
 var server = net.createServer(function(socket) {
-	console.log('server connected');
-
-	// sends a response based a recieved data
-	socket.on('data', function(data) {
-		var ack = '';
-		console.log('recieved: \'' + data + '\'');
-		
-		// respond to "HELLO"
-		if (data == 'HELLO') {
-			socket.write('ACK');
-			console.log('sent: \'ACK\'');
-		}
-		// check for filematch
-		else if (data == 'afile.txt'){
-			var fileStream = fs.createReadStream(FILEPATH + data);
-			
-			// there is an issue with the file
-			fileStream.on('error', function(err){
-				console.log(err);
-				socket.write('NACK: There was a problem retrieving that file');
-				console.log('sent: \'ACK: There was a problem retrieving that file\'');
-			});
-
-			// the file is found and opened 
-			fileStream.on('open',function() {
-				socket.write('FTR');
-				console.log('sent: \'FTR\'');
-				console.log('sending \'' + data +'\'');
-				fileStream.pipe(socket);
-				// BAD:
-				/*
-				var chunk = fileStream.read();
-				if (chunk !== null) {	
-					socket.write(chunk);
-					console.log('file transfer complete');
-				}
-				*/
-			});
-		}
-		else {
-			socket.write('NACK: That file does not exist on our server');
-			console.log('sent: \'ACK: That file does not exist on our server\'');
-		}
-	});
-
+	console.log('client connected');
 });
 
 server.listen(PORT, HOST, function() {
 	console.log('server listen on ' + PORT + '');
 
-	server.on('connection', function() {
+	server.on('connection', function(socket) {
 		console.log('connection made\n');
+		// sends a response based a recieved data
+		socket.on('data', function(data) {
+			var ack = '';
+			console.log('recieved: \'' + data + '\'');
+			
+			// respond to "HELLO"
+			if (data == 'HELLO') {
+				socket.write('ACK');
+				console.log('sent: \'ACK\'');
+			}
+			// check for filematch
+			else if (data == 'afile.txt'){
+				var fileStream = fs.createReadStream(FILEPATH + data);
+				
+				// there is an issue with the file
+				fileStream.on('error', function(err){
+					console.log(err);
+					socket.write('NACK: There was a problem retrieving that file');
+					console.log('sent: \'ACK: There was a problem retrieving that file\'');
+				});
+
+				// the file is found and opened 
+				fileStream.on('open',function() {
+					socket.write('FTR');
+					console.log('sent: \'FTR\'');
+					console.log('sending \'' + data +'\'');
+					fileStream.pipe(socket, {end: false});
+				});
+			}
+			else if (data == 'ACK') {
+				console.log('file transfer complete');
+			}
+			else if (data == 'exit') {
+				socket.end();
+				console.log('connnection closed');
+			}
+			else {
+				socket.write('NACK: That file does not exist on our server');
+				console.log('sent: \'ACK: That file does not exist on our server\'');
+			}
+		});
 	});
 });
